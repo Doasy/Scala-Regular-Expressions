b2344e552b8f51237f6ae15a4c542fe29b29a5d6
Alba Lamas <alba.v.lmas@gmail.com>
2018-04-14 19:21:11 +0200

    done

--- src/src/main/ContactList.scala
+++ src/src/main/ContactList.scala
@@ -1,5 +1,27 @@
 package main
+import scala.io.Source
+import scala.util.matching.Regex
 
-class ContactList {
 
+class ContactList(){
+  def matchWithRegularExpression(line: String){
+
+    var pattern = new Regex("(Name: [A-Z][a-z]+( [A-Z][a-z]*)?( [A-Z][a-z]*)?)|" +
+                            "(email: [a-z]+@[a-z]+\\.[a-z]+)|" +
+                            "(Telf: [0-9]{9})")
+    if ((pattern findAllIn line).mkString(",") == line){
+      println("(OK)"+line)
+    }else{
+      println("(KO)"+line)
+    }
+  }
 }
+object ContactList extends App {
+  val ct = new ContactList()
+
+  val filename = "/home/alba/compiladors/Scala-Regular-Expressions/src/src/docs/testContactList.txt"
+  for (line <- Source.fromFile(filename).getLines) {
+    ct.matchWithRegularExpression(line)
+  }
+}
+
